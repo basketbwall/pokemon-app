05dbef4b2316d1e5cf85d3c5d63b8c51d61a51f5
William <tuj10861@temple.edu>
2022-04-23 19:34:48 -0400

    Got react app to work with pokeapi and learned how to use props and state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//app needs to be rewritten to hold information about caught pokemon
+
+//pass down a click function to the cards that will add to the inventory in app component
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      inventory: [
+        { name: 'ditto', image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"},
+      ],
+      selectedName: null,
+      selectedImage: null
+    };
+  }
+
+  updateInventory = (array) => {
+    this.setState({
+      inventory: array,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <Card updateInventory={() => this.updateInventory(this.state.inventory)} inventory={this.state.inventory}/>
+          <Card updateInventory={() => this.updateInventory(this.state.inventory)} inventory={this.state.inventory}/>
+          <Card updateInventory={() => this.updateInventory(this.state.inventory)} inventory={this.state.inventory}/>
+        </div>
+        <div>
+          <CardStorage inventory={this.state.inventory}/>
+        </div>
+      </div>
+    );
+  }
 }
+
+class CardStorage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Pokemon Collected So Far</h1>
+        <div>
+          {this.props.inventory.map(
+            (item) =>
+            <div>
+              <img src={item.image}></img>
+              <p>{item.name}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+class Card extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      image: null,
+      name: null
+    };
+  }
+
+  componentDidMount() {
+    var pokemonRoute;
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          //take random pokemon from results array
+          var randomIndex = Math.floor(Math.random() * result.results.length);
+          pokemonRoute = result.results[randomIndex].url;
+          fetch(pokemonRoute)
+            .then(res => res.json())
+            .then(
+              (result) => {
+                this.setState({
+                  name: result.name,
+                  image: result.sprites.front_default
+                });
+              }
+            );
+        }
+      );
+    
+  }
+
+  render() {
+    const { image, name } = this.state;
+    return (
+      <div className="card">
+      <div className="card_body">
+        <img 
+          src={this.state.image}
+          className='card_image'>
+        </img>
+        <p className='card_name'>{this.state.name}</p>
+      </div>
+      <button className="card_button" class="btn btn-primary" onClick={() => this.addPokemon()}>Catch Pokemon</button>
+      </div>
+    );
+  };
+
+  addPokemon() {
+    this.props.inventory.push( { image: this.state.image, name: this.state.name });
+    this.props.updateInventory();
+  }
+}
+
+
 
 export default App;
